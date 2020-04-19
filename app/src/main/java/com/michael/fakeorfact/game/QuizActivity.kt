e35c6580747c4fb237c2bb2a3bf75095beb67402
Michael McComas <55296063+McComas-Developer@users.noreply.github.com>
2020-04-19 16:40:54 -0500

    simplified fragment change in Quiz Activity

--- app/src/main/java/com/michael/fakeorfact/game/QuizActivity.kt
+++ app/src/main/java/com/michael/fakeorfact/game/QuizActivity.kt
@@ -14,6 +14,7 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.airbnb.lottie.LottieAnimationView
@@ -237,18 +238,13 @@ class QuizActivity : AppCompatActivity(), View.OnClickListener {
         setButtons()
         val manager = supportFragmentManager
         val transaction = manager.beginTransaction()
-        if(choice == "Fact" && qAns == true || choice == "Fake" && qAns == false) {
-            val correctFragment = CorrectFragment()
-            transaction.replace(R.id.quiz_fragment, correctFragment)
-            transaction.addToBackStack(null)
-            transaction.commit()
-        }
-        else{
-            val correctFragment = WrongFragment()
-            transaction.replace(R.id.quiz_fragment, correctFragment)
-            transaction.addToBackStack(null)
-            transaction.commit()
-        }
+        val fragment: Fragment = if(choice == "Fact" && qAns == true || choice == "Fake" && qAns == false)
+            CorrectFragment()
+        else
+            WrongFragment()
+        transaction.replace(R.id.quiz_fragment, fragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
         next!!.visibility = View.VISIBLE
         explain!!.visibility = View.VISIBLE
     }
